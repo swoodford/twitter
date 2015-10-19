82dc1e349bca085fb3ad0e12b6767b7912bb0cae
Shawn Woodford <sw@shawnwoodford.com>
2015-10-19 15:50:52 -0400

    test if credentials set

--- twitter_api_creds.py
+++ twitter_api_creds.py
@@ -8,3 +8,8 @@ os.environ["CONSUMER_SECRET"] = "YOUR KEY HERE"
 os.environ["ACCESS_KEY"] = "YOUR KEY HERE"
 os.environ["ACCESS_SECRET"] = "YOUR KEY HERE"
 
+if os.environ["CONSUMER_KEY"] == "YOUR KEY HERE":
+    print "You failed to configure your Twitter API credentials in the file twitter_api_creds.py!"
+    exit()
+else:
+    pass
